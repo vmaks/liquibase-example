64305f81c7e1934e59883344537bf27a6c80e987
Maksim Vlasov <vlasov.maksim@hotmail.com>
2020-09-18 20:39:09 +0300

    Add simple spring maven project

---
+++ src/main/kotlin/com/maks/liquibaseexample/LiquibaseexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.maks.liquibaseexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiquibaseexampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<LiquibaseexampleApplication>(*args)
+}
